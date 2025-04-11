2dc063d7221431afc4e3e38ee06ec62ed8bfa3f5
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-04-11 14:32:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+() => {
+  // Confirm the modal is not visible (no .show class)
+  cy.get('#signupModal').should('not.have.class', 'show');
+
+  // Click the button to trigger the modal
+  cy.get('button').first().click();
+
+  // Assert the modal has the .show class (Bootstrap applies when visible)
+  cy.get('#signupModal').should('have.class', 'show');
+}
